f0b52609cb33f68e06e11b1ecbe8ec2fa8ddb8bd
flintgr <flintgr@gmail.com>
2026-07-08 17:25:27 +0300

    Sprint 1.3 Home v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "family-budget-sprint-1-2-1";
+const CACHE_NAME = "family-budget-sprint-1-3-home-v2";
 self.addEventListener("install", event => {
   self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(["./","./index.html","./style.css","./config.js","./app.js","./manifest.json"])));
